44fedd82649bb76e430cd68c21dd104577a6179e
LaSe28 <mattia.las28@gmail.com>
2022-03-29 18:34:51 +0200

    end search

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
     <HeaderBool @search="searchTitle"/>
-    <MainBool :textSearchFor="searchValue"/>
+    <MainBool :textSearchFor="arrSearch"/>
   </div>
 </template>
 
 <script>
 import HeaderBool from './components/HeaderBoolflix.vue'
 import MainBool from './components/MainBoolflix.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   data () {
     return {
-      searchValue: ''
+      searchValue: '',
+      arrSearch: []
     }
   },
   components: {
@@ -22,7 +24,23 @@ export default {
   },
   methods: {
     searchTitle (searchValue) {
+      this.arrSearch = []
       this.searchValue = searchValue
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=01af620fbe2924c05e6048caa6f5c225&language=it_IT&query=${this.searchValue}`)
+        .then((res) => {
+          for (let i = 0; i < res.data.results.length; i++) {
+            this.arrSearch.push({
+              title: res.data.results[i].title,
+              overview: res.data.results[i].overview,
+              poster_path: res.data.results[i].poster_path,
+              original_title: res.data.results[i].original_title,
+              original_language: res.data.results[i].original_language,
+              vote_average: res.data.results[i].vote_average
+            })
+          }
+          console.log(this.arrSearch)
+          return this.arrSearch
+        })
     }
   }
 }
--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -1,47 +1,46 @@
 <template>
-  <main :textSearchFor="searched()">
-    <div v-for="film in arrSearch" :key="film.title">
-      <div>{{film.title}}</div>
+  <main>
+    <div class="card" v-for="film in textSearchFor" :key="film.title">
+      <div>Titolo: {{film.title}}</div>
+      <div>Titolo originale: {{film.original_title}}</div>
+      <div>Trama: {{film.overview}}</div>
+      <div>Voto: {{film.vote_average}}</div>
+
     </div>
   </main>
 </template>
 
 <script>
-import axios from 'axios'
 
 export default {
   name: 'MainBool',
   data () {
     return {
-      arrSearch: []
     }
   },
   props: {
-    textSearchFor: String
+    textSearchFor: Array
   },
   methods: {
-    searched () {
-      if (this.textSearchFor !== '') {
-        console.log(this.textSearchFor)
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=01af620fbe2924c05e6048caa6f5c225&query=${this.textSearchFor}`)
-          .then((res) => {
-            console.log(res.data.results)
-            for (let i = 0; i < res.data.results.length; i++) {
-              this.arrSearch.push({
-                title: res.data.results[i].title,
-                overview: res.data.results[i].overview,
-                poster_path: res.data.results[i].poster_path,
-                original_title: res.data.results[i].original_title
-              })
-            }
-            console.log(this.arrSearch)
-          })
-      }
-    }
+
   }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+main{
+  display: flex;
+  flex-wrap: wrap;
+}
+.card{
+  height: 300px;
+  max-width: calc((100% / 6) - 2rem);
+  min-width: 200px;
+  margin: 2rem;
+  overflow: auto;
+  background-color: #c1c1c1;
+  div{
+    margin-bottom: 1rem;
+  }
+}
 </style>
