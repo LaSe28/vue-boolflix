2d49bd153e21852a0055bd62165b22a13a948103
LaSe28 <mattia.las28@gmail.com>
2022-03-29 16:15:43 +0200

    dont know

--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -1,6 +1,8 @@
 <template>
-  <main>
-    <p :textSearchFor="searched()"></p>
+  <main :textSearchFor="searched()">
+    <div v-for="film in arrSearch" :key="film.title">
+      <div>{{film.title}}</div>
+    </div>
   </main>
 </template>
 
@@ -11,7 +13,7 @@ export default {
   name: 'MainBool',
   data () {
     return {
-      searchedFor: ''
+      arrSearch: []
     }
   },
   props: {
@@ -22,7 +24,18 @@ export default {
       if (this.textSearchFor !== '') {
         console.log(this.textSearchFor)
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=01af620fbe2924c05e6048caa6f5c225&query=${this.textSearchFor}`)
-          .then((res) => console.log(res.data.results))
+          .then((res) => {
+            console.log(res.data.results)
+            for (let i = 0; i < res.data.results.length; i++) {
+              this.arrSearch.push({
+                title: res.data.results[i].title,
+                overview: res.data.results[i].overview,
+                poster_path: res.data.results[i].poster_path,
+                original_title: res.data.results[i].original_title
+              })
+            }
+            console.log(this.arrSearch)
+          })
       }
     }
   }
