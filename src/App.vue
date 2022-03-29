ee163a8edda9796c5032688e5ee7be76748e5625
LaSe28 <mattia.las28@gmail.com>
2022-03-29 13:10:50 +0200

    search

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
 <script>
 import HeaderBool from './components/HeaderBoolflix.vue'
 import MainBool from './components/MainBoolflix.vue'
-import axios from 'axios'
 
 export default {
   name: 'App',
@@ -24,8 +23,6 @@ export default {
   methods: {
     searchTitle (searchValue) {
       this.searchValue = searchValue
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=01af620fbe2924c05e6048caa6f5c225&query=${this.searchValue}`)
-        .then((res) => console.log(res.data.results))
     }
   }
 }
--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -1,10 +1,12 @@
 <template>
   <main>
-    <p></p>
+    <p :textSearchFor="searched()"></p>
   </main>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'MainBool',
   data () {
@@ -14,6 +16,15 @@ export default {
   },
   props: {
     textSearchFor: String
+  },
+  methods: {
+    searched () {
+      if (this.searchedFor !== '') {
+        console.log(this.textSearchFor)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=01af620fbe2924c05e6048caa6f5c225&query=${this.textSearchFor}`)
+          .then((res) => console.log(res.data.results))
+      }
+    }
   }
 }
 </script>
